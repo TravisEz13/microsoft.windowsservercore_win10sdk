fccaac9e93aa832d20bd2d3b8522c77a85852703
Travis Plunk <travis.plunk@microsoft.com>
2017-04-07 17:49:39 -0700

    Use new module to install

--- install_win10sdk.ps1
+++ install_win10sdk.ps1
@@ -1,34 +1,11 @@
-Add-Type -OutputAssembly net.exe -OutputType ConsoleApplication -TypeDefinition @"
-class Program 
+Import-Module "$psscriptroot\dockerinstall.psm1" -DisableNameChecking
+Install-FakeNetExe
+try 
 {
-  static void Main(string[] args)
-  {
-  }
+    Install-ChocolateyPackage -PackageName "windows-sdk-10.1" -CleanUp
+    Remove-Win10SdkPackageCache
 }
-"@
-
-Copy-Item C:\Windows\System32\net.exe C:\Windows\System32\net.exe.bak
-Copy-Item C:\Windows\SysWOW64\net.exe C:\Windows\SysWOW64\net.exe.bak
-$acl1 = Get-Acl C:\Windows\System32\net.exe
-$acl11 = Get-Acl C:\Windows\System32\net.exe
-$acl2 = Get-Acl C:\Windows\SysWOW64\net.exe
-$acl21 = Get-Acl C:\Windows\SysWOW64\net.exe
-$permission = 'BUILTIN\Administrators','FullControl','Allow'
-$accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
-$acl11.SetAccessRule($accessRule)
-Set-Acl C:\Windows\System32\net.exe -AclObject $acl11
-$acl21.SetAccessRule($accessRule)
-Set-Acl C:\Windows\SysWOW64\net.exe -AclObject $acl21
-Copy-Item .\net.exe C:\Windows\System32\net.exe
-Copy-Item .\net.exe C:\Windows\SysWOW64\net.exe
-
-Write-Verbose "Installing win10 sdk..." -Verbose
-Install-PackageProvider -Name Chocolatey -Force
-Install-Package -Name windows-sdk-10.1 -ProviderName Chocolatey -Force
-
-Remove-Item C:\Windows\System32\net.exe
-Remove-Item C:\Windows\SysWOW64\net.exe
-Rename-Item C:\Windows\System32\net.exe.bak C:\Windows\System32\net.exe
-Rename-Item C:\Windows\SysWOW64\net.exe.bak C:\Windows\SysWOW64\net.exe
-Set-Acl C:\Windows\System32\net.exe -AclObject $acl1
-Set-Acl C:\Windows\SysWOW64\net.exe -AclObject $acl2
+finally
+{
+    Remove-FakeNetExe  
+}
