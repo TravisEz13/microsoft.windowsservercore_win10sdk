6fd6d469a1a27fbc1777ed0e26683221fa0f136c
Travis Plunk <travis.plunk@microsoft.com>
2017-04-07 17:50:24 -0700

    add local build script

---
+++ build.ps1
@@ -0,0 +1,19 @@
+param(
+    [string[]]
+    $Tags = 'latest',
+    [switch]
+    $NoCache
+)
+$repoName = "$psscriptroot\reponame.ps1"
+$tagParams = @()
+foreach($tag in $tags)
+{
+    $tag = $tag.ToLowerInvariant()
+    $tagParams += '-t'
+    $tagParams += "${repoName}:$tag"
+}
+if($NoCache)
+{
+    $tagParams += '--no-cache'
+}
+docker build $tagParams $PSScriptRoot
