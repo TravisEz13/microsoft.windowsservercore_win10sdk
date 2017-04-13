e6ff44974d745e9317ef02188f6a363416896bd4
Travis Plunk <travis.plunk@microsoft.com>
2017-04-13 14:31:10 -0700

    merge dockerfiles from build-tools

--- dockerInstall.psm1
+++ dockerInstall.psm1
@@ -15,8 +15,11 @@ function Install-ChocolateyPackage
 
         [Parameter(Mandatory=$false)]
         [string]
-        $Executable
-        ,
+        $Executable,
+
+        [string[]]
+        $ArgumentList,
+
         [switch]
         $Cleanup
     )
@@ -28,7 +31,7 @@ function Install-ChocolateyPackage
     }
 
     Write-Verbose "Installing $PackageName..." -Verbose
-    choco install -y $PackageName
+    choco install -y $PackageName $ArgumentList
 
     if($executable)
     {
@@ -146,5 +149,34 @@ function Remove-FakeNetExe {
     }
     else {
         Write-Warning "!!!Syswow64\net.exe acl was not saved!!!"
+    }
+}
+
+$cmdContents = @"
+@echo off
+call "%ProgramFiles(x86)%\Microsoft Visual C++ Build Tools\vcbuildtools.bat" x64
+set
+"@
+function Set-SdkEnv
+{
+    $cmdPath = Join-Path $env:temp -ChildPath ([System.IO.Path]::GetRandomFileName() + '.cmd')
+    Set-Content -LiteralPath $cmdPath -Value $cmdContents -Encoding Ascii 
+    try
+    {
+        &$cmdPath | Foreach-Object {
+            $cmdVar,$cmdVal=$_.split('=')
+            if($cmdVar -ne 'Platform')
+            {
+                if((Get-Item -Path env:$cmdVar -Exclude SilentlyContinue).Value -ne $cmdVal)
+                {
+                    Write-Verbose "setting machine variable $cmdVar to $cmdVal" -Verbose
+                    [System.Environment]::SetEnvironmentVariable($cmdVar,$cmdVal,[System.EnvironmentVariableTarget]::Machine)
+                }
+            }
+        }
+    }
+    finally
+    {
+        Remove-Item -LiteralPath $cmdPath -ErrorAction SilentlyContinue -Force
     }
 }
